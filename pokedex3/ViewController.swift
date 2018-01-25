e83279e9602b6727660881c6a02ce50cd19edb1a
Simon Salmon <simonsalmon@simons-mbp.local.orangesteel.org>
2018-01-25 20:23:07 +0000

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Simon Salmon on 25/01/2018.
+//  Copyright © 2018 Simon Salmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
